c223cc27a84c8930ee1daff43c491c358469b8ae
fishstik <by34@pm.me>
2021-06-07 00:59:05 -0400

    Add per-server region disable

--- bot.py
+++ bot.py
@@ -17,6 +17,27 @@ def exit():
     print('Exiting...')
 atexit.register(exit)
 
+CFG_FILENAME = 'config.ini'
+DB_DIR = './dbs'
+BOT_PREFIX = ('!lelnovo ')
+EMBED_COLOR = 0xe41c1c
+DISABLED_REGIONS = {
+    851248442864173057: ['tck'], # lbt2
+    361360173530480640: ['tck'], # SAL
+}
+
+CMD_ALIASES = {
+    'h': 'help',
+    'lr': 'listregions',
+    'st': 'status',
+}
+REGCMD_ALIASES = {
+    'st': 'status',
+    'ls': 'listspecs',
+    's':  'search',
+    'sp': 'specs',
+}
+
 class FileHandler(FileSystemEventHandler):
     def on_modified(self, event):
         global dbs
@@ -30,22 +51,6 @@ class FileHandler(FileSystemEventHandler):
                 except json.decoder.JSONDecodeError:
                     print(f'JSON load error. Retrying ({i+1}/5)...')
                     time.sleep(1)
-
-CFG_FILENAME = 'config.ini'
-DB_DIR       = './dbs'
-BOT_PREFIX   = ('!lelnovo ')
-EMBED_COLOR  = 0xe41c1c
-CMD_ALIASES = {
-    'h': 'help',
-    'lr': 'listregions',
-    'st': 'status',
-}
-REGCMD_ALIASES = {
-    'st': 'status',
-    'ls': 'listspecs',
-    's':  'search',
-    'sp': 'specs',
-}
 
 bot = discord.ext.commands.Bot(
     command_prefix=BOT_PREFIX,
@@ -75,133 +80,138 @@ async def on_ready():
 # need to add command for every region
 @bot.command()
 async def tck(context, *args):
-    embed = parse_command(args, region='tck')
+    embed = parse_command(context, args, region='tck')
     if embed: await context.send(embed=embed)
 
 @bot.command()
 async def us(context, *args):
-    embed = parse_command(args, region='us')
+    embed = parse_command(context, args, region='us')
     if embed: await context.send(embed=embed)
 
-def parse_command(args, region):
-    command = args[0]
-    params = args[1:]
+def parse_command(context, args, region):
+    guild_id = context.guild.id
     embed = None
 
-    db = dbs[region]
-    region_emoji = lelnovo.get_region_emoji(db['metadata']['short region'])
+    if guild_id in DISABLED_REGIONS and region in DISABLED_REGIONS[guild_id]:
+        print(f'Guild \'{context.guild}\' got disabled region command \'{region}\'. Ignoring...')
+    else:
+        command = args[0]
+        params = args[1:]
 
-    if command in ['status', 'st']:
-        embed = discord.Embed(
-            title='Database Status',
-            description=lelnovo.get_status(db),
-            color=EMBED_COLOR,
-        )
-        embed.set_footer(text = lelnovo.get_footer(db))
-    elif command in ['listspecs', 'ls']:
-        embed = discord.Embed(
-            title='Specs List',
-            description='All specs that can be used in `search` and `specs` commands\n',
-            color=EMBED_COLOR,
-        )
-        contents = ''
-        specs = sorted(db['keys']['info'])
-        contents = '```'
-        for i in range(0, len(specs), 3):
-            contents += ('  '+' '.join([f'{spec:20}' for spec in specs[i:i+3]])+'\n')
-        contents += '```'
-        embed.add_field(name='specs', value=contents, inline=False)
+        db = dbs[region]
+        region_emoji = lelnovo.get_region_emoji(db['metadata']['short region'])
 
-        specs = sorted(db['keys']['num_specs'])
-        contents = 'These specs contain numbers that can be used in a numeric `search` condition\n'
-        contents += '```'
-        for i in range(0, len(specs), 2):
-            contents += ('  '+' '.join([f'{spec:20}' for spec in specs[i:i+2]])+'\n')
-        contents += '```'
-        embed.add_field(name='number specs', value=contents, inline=False)
-
-        embed.set_footer(text = lelnovo.get_footer(db))
-    elif command in ['search', 's']:
-        params = ' '.join(params)
-        if params:
-            count = 0
-
-            results, error = lelnovo.search(params, db)
-            if error:
-                embed = discord.Embed(
-                    title = f'{region_emoji} Search Failed',
-                    description = f'Invalid query `{params}` (check commas!)',
-                    color=EMBED_COLOR,
-                )
-            else:
-                embed = discord.Embed(
-                    title = f'{region_emoji} Search Results for \'{params}\'',
-                    color=EMBED_COLOR,
-                )
-                for result in results:
-                    prod   = result[0]
-                    pn     = result[1]['part number']
-                    price  = result[1]['num_specs']['price']
-                    status = result[1]['status']
-                    spec_matches = result[2]
-
-                    contents = f'{pn} ([link]({db["metadata"]["base url"]}/p/{pn}))'
-
-                    price_str = f'{price[1]}{price[0]:.2f}'
-                    if status.lower() == 'unavailable':
-                        contents += f' **~~{price_str}~~ (unavailable)**\n'
-                    elif status.lower() == 'customize':
-                        contents += f' **{price_str} (customize)**\n'
-                    else:
-                        contents += f' **{price_str}**\n'
-
-                    spacing = max([len(k[0]) for k in spec_matches])
-                    for match in result[2]:
-                        contents += lelnovo.multiline(f'{match[0]:{spacing}}  {match[1]}', indent=spacing+2) + '\n'
-                    embed.add_field(
-                        name = result[1]['name'],
-                        value = contents,
-                        inline = False,
-                    )
-                    count += 1
-                    if count == 10:
-                        break
-
-                summary = f'Found **{len(results)}** results for `{params}`'
-                if len(results) > 10: summary += ' (only showing first 10)'
-                embed.add_field(
-                    name = '\u200b',
-                    value = summary,
-                )
+        if command in ['status', 'st']:
+            embed = discord.Embed(
+                title='Database Status',
+                description=lelnovo.get_status(db),
+                color=EMBED_COLOR,
+            )
             embed.set_footer(text = lelnovo.get_footer(db))
-    elif command in ['specs', 'sp']:
-        params = ' '.join(params)
-        if params:
-            specs = []
-            words = re.split('\s+', params, 1)
-            part_num = words[0]
-            # user-provided specs
-            if len(words) > 1: specs = [s.strip() for s in words[1].split(',')]
-            res = lelnovo.get_specs(part_num, db, specs)
+        elif command in ['listspecs', 'ls']:
+            embed = discord.Embed(
+                title='Specs List',
+                description='All specs that can be used in `search` and `specs` commands\n',
+                color=EMBED_COLOR,
+            )
+            contents = ''
+            specs = sorted(db['keys']['info'])
+            contents = '```'
+            for i in range(0, len(specs), 3):
+                contents += ('  '+' '.join([f'{spec:20}' for spec in specs[i:i+3]])+'\n')
+            contents += '```'
+            embed.add_field(name='specs', value=contents, inline=False)
 
-            if res:
-                info, ret_specs = res
-                if info and ret_specs:
+            specs = sorted(db['keys']['num_specs'])
+            contents = 'These specs contain numbers that can be used in a numeric `search` condition\n'
+            contents += '```'
+            for i in range(0, len(specs), 2):
+                contents += ('  '+' '.join([f'{spec:20}' for spec in specs[i:i+2]])+'\n')
+            contents += '```'
+            embed.add_field(name='number specs', value=contents, inline=False)
+
+            embed.set_footer(text = lelnovo.get_footer(db))
+        elif command in ['search', 's']:
+            params = ' '.join(params)
+            if params:
+                count = 0
+
+                results, error = lelnovo.search(params, db)
+                if error:
                     embed = discord.Embed(
-                        title=f'{region_emoji} Specs for {info["name"]}',
-                        description=lelnovo.format_specs(db, info, ret_specs)[:2048],
+                        title = f'{region_emoji} Search Failed',
+                        description = f'Invalid query `{params}` (check commas!)',
                         color=EMBED_COLOR,
                     )
-            else:
-                embed = discord.Embed(
-                    title=f'{region_emoji} Specs for \'{part_num}\' not found',
-                    description=f'Check that the part number is valid. Discontinued products are not in database.',
-                    color=EMBED_COLOR,
-                )
+                else:
+                    embed = discord.Embed(
+                        title = f'{region_emoji} Search Results for \'{params}\'',
+                        color=EMBED_COLOR,
+                    )
+                    for result in results:
+                        prod   = result[0]
+                        pn     = result[1]['part number']
+                        price  = result[1]['num_specs']['price']
+                        status = result[1]['status']
+                        spec_matches = result[2]
 
-            embed.set_footer(text = lelnovo.get_footer(db))
-    else:
-        print(f'Unrecognized command \'{" ".join(args)}\'')
+                        contents = f'{pn} ([link]({db["metadata"]["base url"]}/p/{pn}))'
+
+                        price_str = f'{price[1]}{price[0]:.2f}'
+                        if status.lower() == 'unavailable':
+                            contents += f' **~~{price_str}~~ (unavailable)**\n'
+                        elif status.lower() == 'customize':
+                            contents += f' **{price_str} (customize)**\n'
+                        else:
+                            contents += f' **{price_str}**\n'
+
+                        spacing = max([len(k[0]) for k in spec_matches])
+                        for match in result[2]:
+                            contents += lelnovo.multiline(f'{match[0]:{spacing}}  {match[1]}', indent=spacing+2) + '\n'
+                        embed.add_field(
+                            name = result[1]['name'],
+                            value = contents,
+                            inline = False,
+                        )
+                        count += 1
+                        if count == 10:
+                            break
+
+                    summary = f'Found **{len(results)}** results for `{params}`'
+                    if len(results) > 10: summary += ' (only showing first 10)'
+                    embed.add_field(
+                        name = '\u200b',
+                        value = summary,
+                    )
+                embed.set_footer(text = lelnovo.get_footer(db))
+        elif command in ['specs', 'sp']:
+            params = ' '.join(params)
+            if params:
+                specs = []
+                words = re.split('\s+', params, 1)
+                part_num = words[0]
+                # user-provided specs
+                if len(words) > 1: specs = [s.strip() for s in words[1].split(',')]
+                res = lelnovo.get_specs(part_num, db, specs)
+
+                if res:
+                    info, ret_specs = res
+                    if info and ret_specs:
+                        embed = discord.Embed(
+                            title=f'{region_emoji} Specs for {info["name"]}',
+                            description=lelnovo.format_specs(db, info, ret_specs)[:2048],
+                            color=EMBED_COLOR,
+                        )
+                else:
+                    embed = discord.Embed(
+                        title=f'{region_emoji} Specs for \'{part_num}\' not found',
+                        description=f'Check that the part number is valid. Discontinued products are not in database.',
+                        color=EMBED_COLOR,
+                    )
+
+                embed.set_footer(text = lelnovo.get_footer(db))
+        else:
+            print(f'Unrecognized command \'{" ".join(args)}\'')
 
     return embed
 
@@ -231,14 +241,17 @@ async def cmd_help(context, *args):
     description = lelnovo.command_descrs['listregions'],
 )
 async def cmd_listregions(context):
+    guild_id = context.guild.id
+
     contents = ''
     for _, db in dbs.items():
         region = db['metadata']['region']
         region_short = db['metadata']['short region']
 
-        contents += f'`{region_short:3}` {lelnovo.get_region_emoji(region_short)}'
-        contents += f' [{region}]({db["metadata"]["base url"]})'
-        contents += '\n'
+        if not (guild_id in DISABLED_REGIONS and region_short in DISABLED_REGIONS[guild_id]):
+            contents += f'`{region_short:3}` {lelnovo.get_region_emoji(region_short)}'
+            contents += f' [{region}]({db["metadata"]["base url"]})'
+            contents += '\n'
 
     embed = discord.Embed(
         title='Region List',
@@ -253,33 +266,37 @@ async def cmd_listregions(context):
     description='display status for all available databases',
 )
 async def cmd_status(context):
+    guild_id = context.guild.id
+
     embed = discord.Embed(
         title='All Database Statuses',
         color=EMBED_COLOR,
     )
     for k, db in dbs.items():
-        contents = ''
-        contents += lelnovo.get_status(db)
+        region = db['metadata']['short region']
+        if not (guild_id in DISABLED_REGIONS and region in DISABLED_REGIONS[guild_id]):
+            contents = ''
+            contents += lelnovo.get_status(db)
 
-        footer = lelnovo.get_footer(db)
-        # remove divider line
-        footer = '\n'.join(footer.split('\n')[1:])
-        contents += f'{footer}\n'
+            footer = lelnovo.get_footer(db)
+            # remove divider line
+            footer = '\n'.join(footer.split('\n')[1:])
+            contents += f'{footer}\n'
 
-        embed.add_field(name=db['metadata']['short region'], value=contents, inline=False)
+            embed.add_field(name=db['metadata']['short region'], value=contents, inline=False)
 
     await context.send(embed=embed)
 
 if __name__ == '__main__':
-    cfg = configparser.ConfigParser()
+    CFG = configparser.ConfigParser()
     if not os.path.exists(CFG_FILENAME):
-        cfg.add_section('discord')
-        cfg.set('discord', 'token', '')
-        with open(CFG_FILENAME, 'w') as cfg_file: cfg.write(cfg_file)
+        CFG.add_section('discord')
+        CFG.set('discord', 'token', '')
+        with open(CFG_FILENAME, 'w') as cfg_file: CFG.write(cfg_file)
         print(f'Created template \'{CFG_FILENAME}\'. Add bot token and restart.')
         sys.exit()
 
-    cfg.read(CFG_FILENAME)
-    token = cfg['discord']['token']
+    CFG.read(CFG_FILENAME)
+    token = CFG['discord']['token']
     if token: bot.run(token)
     else:     print(f'Token not found in \'{CFG_FILENAME}\'')
